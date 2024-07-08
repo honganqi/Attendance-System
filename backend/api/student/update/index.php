14e37bb92c6461ff378765377652a22aa1840741
Archie <honganqi@gmail.com>
2024-07-09 00:27:51 +0800

    Backend: Fix delete student response

--- backend/api/student/update/index.php
+++ backend/api/student/update/index.php
@@ -9,19 +9,17 @@ if (isset($_GET['id'])) {
     $returnjson = array();
     $student = new Student($_GET['id']);
     $contents = json_decode(trim(file_get_contents("php://input")), true);
-    $newData = $contents['newData'];
-    if (isset($newData)) {
-        $response = $student->updateRecord($newData);
-    } else {
-        $delete = $contents['delete'];
-        if (isset($delete)) {
-            $response = $student->deleteRecord();
-        }
+    if (array_key_exists('newData', $contents)) {
+        $response = $student->updateRecord($contents['newData']);
+    } elseif (array_key_exists('delete', $contents)) {
+        $response = $student->deleteRecord();
     }
+
+    $verb = array_key_exists('delete', $contents) ? "deleted" : "updated";
 
     header($response['status_code_header']);
     if ($response['body']) {
-        $response['message'] = "Student record updated successfully!";
+        $response['message'] = "Student record $verb successfully!";
         $response['messageType'] = "success";
         echo json_encode($response);
     }
